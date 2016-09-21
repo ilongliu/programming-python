ba03fa70200b764da5e6f80739c822c1feb18fe6
ilongliu <ilongliu@yeah.net>
2016-09-21 20:56:22 +0800

    Update manager.py

--- part I/manager.py
+++ part I/manager.py
@@ -4,6 +4,7 @@ class Manager(Person):
     def giveRaise(self, percent, bonus=0.1):
         self.pay *= (1.0 + percent + bonus)
 
+
 if __name__ == '__main__':
     tom = Manager(name='Tom Doe', age=50, pay=50000)
     print(tom.lastName())
